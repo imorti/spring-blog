dc3af589ca61b2082b718bc5dd3737a0fb3bf582
Ian <imortimer@apprenda.com>
2015-12-08 17:11:18 -0500

    added blog, item and role entities.

--- src/main/java/com/imorti/jazzhands/entity/User.java
+++ src/main/java/com/imorti/jazzhands/entity/User.java
@@ -1,8 +1,12 @@
 package com.imorti.jazzhands.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 
 @Entity
@@ -11,6 +15,40 @@ public class User {
 	@Id
 	@GeneratedValue
 	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	@ManyToMany
+	@JoinTable
+	private List<Role> roles;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public Integer getId() {
 		return id;
