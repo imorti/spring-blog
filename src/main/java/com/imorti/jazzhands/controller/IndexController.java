167bfc4280efe9a5fb3f08fd60b79a0009d6a8f2
Ian <imortimer@apprenda.com>
2015-12-08 16:21:10 -0500

    added logger

--- src/main/java/com/imorti/jazzhands/controller/IndexController.java
+++ src/main/java/com/imorti/jazzhands/controller/IndexController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+import java.util.logging.Logger;
+
 /**
  * @author imorti
  *
@@ -16,6 +18,11 @@ public class IndexController {
 	
 	@RequestMapping("/index")
 	public String index() {
+		
+		final Logger logger = Logger.getLogger("IndexController");
+		
+		logger.info("responding to IndexController request");
+		
 		return "index";
 	}
 	
