7f3c3d796d014b617055bbf1c8651ff5d03bec21
Ian <imortimer@apprenda.com>
2015-12-09 17:10:48 -0500

    lazy loading blog information

--- src/main/java/com/imorti/jazzhands/entity/User.java
+++ src/main/java/com/imorti/jazzhands/entity/User.java
@@ -3,6 +3,7 @@ package com.imorti.jazzhands.entity;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinTable;
@@ -23,7 +24,7 @@ public class User {
 	
 	private String password;
 	
-	@OneToMany(mappedBy = "user")
+	@OneToMany(mappedBy = "user", fetch=FetchType.LAZY)
 	private List<Blog> blogs;
 	
 	public List<Blog> getBlogs() {
